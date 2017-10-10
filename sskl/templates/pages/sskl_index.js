842f4a32257a01bb08e33e10495b1e4d3a9b3860
bella2 <10291530115@qq.com>
2017-10-10 13:42:13 +0800

    百度地图的完善

--- sskl/templates/pages/sskl_index.js
+++ sskl/templates/pages/sskl_index.js
@@ -1,13 +1,16 @@
 navList("资源管理");
-	var map = new BMap.Map("map",{enableMapClick: false});
-	map.centerAndZoom("北京市", 10);
+    var map = new BMap.Map("map",{enableMapClick: false});
+	map.centerAndZoom("北京市", 5);
 	map.enableScrollWheelZoom();
+
 	var opts = {
-		width : 250,     // 信息窗口宽度
-		height: 145,     // 信息窗口高度
+		width : 260,     // 信息窗口宽度
+		height: 88,     // 信息窗口高度
 		//title : "设备信息" , // 信息窗口标题
 		enableMessage:true//设置允许信息窗发送短息
 	};
+
+
 	function addClickHandler(content,marker){
 		marker.addEventListener("click",function(e){
 			openInfo(content,e)}
@@ -23,18 +26,16 @@ navList("资源管理");
 
 
 $(function(){
-	console.log(parseInt($(window).height())-86);
+	console.log(parseInt($(window).height()));
 	console.log($(document.body).height());
 	console.log(parseInt($('div.main-container').offsetHeight));
-	$('#l-map').height(parseInt($(window).height())-86);
+	$('#map').height(parseInt($(window).height()));
 	$(window).resize(function() {
-	  $('#l-map').height(parseInt($(window).height())-86);
+	  $('#map').height(parseInt($(window).height()));
 	});
-    $("#footerandfooter").remove();
-    var dataurl = "/api/method/iot_ui.ui_api.devices_list_array?filter=all";
+    // $(".footer").remove();
+    var dataurl = "http://192.168.174.140:8000/api/method/tieta.tieta.doctype.cell_station.cell_station.search_station?page_length=9999";
     $.ajax({url:dataurl,async:true,success:function(r){
-        console.log(r);
-
         //################################################
 
         if(r.message.length){
@@ -42,15 +43,23 @@ $(function(){
             var stations = r.message;
             for (var cs in stations) {
                 pt = new BMap.Point(stations[cs].longitude, stations[cs].latitude);
-                var myIcon = new BMap.Icon("/assets/iot_ui/assets/images/symgate.png", new BMap.Size(32,32));
+                var myIcon = new BMap.Icon("/assets/sskl/images/jizhan.png", new BMap.Size(32,38));
                 var marker = new BMap.Marker(pt,{icon:myIcon});
-                var gourl = "/iot_devinfo/" + stations[cs].device_sn;
-                var content = "<div class='widget-box'><div class='widget-header'><h4 class='widget-title'>"+ stations[cs].device_name +"</h4><div class='widget-toolbar'></div></div><div class='widget-body'><div class='widget-main'><div><a class='header smaller lighter black' style='text-decoration:none;' href='/iot_devinfo/"+ stations[cs].device_sn +"'>" + "<p> 描述 : " + stations[cs].device_desc + "</p>" +"<p> 归属 : " + stations[cs].device_company + "</p></a></div></div></div></div>"
-                    addClickHandler(content, marker);
-                markers.push(marker);
+                // var gourl = "/iot_devinfo/" + stations[cs].device_sn;
+
+                var content =
+                    "<div class='widget-box'>" +
+                        "<h4 class='widget-title'>"+ stations[cs].station_name +"</h4>" +
+                        "<p> 地&nbsp;&nbsp;&nbsp;址:"+ stations[cs].address_text +"</p>"+
+                         "<p> 联系人:后台需添加</p>"+"<a href='sskl_netWork.html'>详情</a>"
+                    "</div>";
+
+                 addClickHandler(content, marker)
+                   markers.push(marker);
             }
             //最简单的用法，生成一个marker数组，然后调用markerClusterer类即可。
-            var markerClusterer = new BMapLib.MarkerClusterer(map, {markers: markers});
+            var markerClusterer = new BMapLib.MarkerClusterer(map, {markers: markers})
+
         }
 
 
@@ -58,4 +67,6 @@ $(function(){
 
             }});
         });
+
+
 
