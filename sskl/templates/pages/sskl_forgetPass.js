3e7b3d923634428555b7ba30370936324f05aee9
bella2 <10291530115@qq.com>
2017-11-07 17:08:54 +0800

    登录及注册更新

---
+++ sskl/templates/pages/sskl_forgetPass.js
@@ -0,0 +1,71 @@
+$(function () {
+        $("#tabs").tabs();
+    });
+$(".loginInUl>li").click(function(){
+    $(this).css("backgroundColor","#fff").siblings("li").css("backgroundColor","#dde0e0");
+});
+
+
+
+
+//验证码
+ window.onload = function(){
+        createCode();
+    }
+function createCode(){
+    FourCode = "";
+    var codeLength = 4;//验证码的长度;
+    var random = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R', 'S','T','U','V','W','X','Y','Z');//随机数
+    for(var i = 0; i < codeLength; i++) {//循环操作
+        var index = Math.floor(Math.random()*36);//取得随机数的索引（0~35）
+         // FourCode += random[index];//根据索引取得随机数加到code上
+         var a="<b class='p2'>"+random[index]+"</b>";
+          FourCode += a;//根据索引取得随机数加到code上
+
+    }
+    $("#FourCode").html(FourCode);
+    $(".p2").css({
+        "transform":"rotate(7deg)"
+    });
+}
+
+$("#dynaminVal").blur(function(){
+    validate();
+});
+function validate(){
+    var inputCode = $("#dynaminVal").val().toUpperCase(); //取得输入的验证码并转化为大写
+    if(inputCode.length <= 0) { //若输入的验证码长度为0
+      // alert("请输入验证码");
+        $("#ErrMessages").show().html("请输入验证码");
+    }
+    else if(inputCode != FourCode ) { //若输入的验证码与产生的验证码不一致时
+        // alert("验证码输入错误！@_@"); //则弹出验证码输入错误
+         $("#ErrMessages").show().html("验证码输入错误");
+        createCode();//刷新验证码
+        $("#dynaminVal").value = "";//清空文本框
+    }
+    else { //输入正确时
+         $("#ErrMessages").hide();
+        // alert("输入正确"); //弹出^-^
+    }
+}
+
+
+//忘记密码输入
+
+$("#inputVerCode").blur(function(){
+        var upwd= $.trim($('#inputVerCode').val());
+        console.log(upwd);
+        var regUpwd=/^[A-Za-z0-9]{6,12}$/;
+        if(!upwd){//当密码为空的时候
+            $('#ForgetErrPassword').show().text("密码不能为空");
+            return false;
+        }
+        else if(!regUpwd.test(upwd)){
+            $('#ForgetErrPassword').show().text("密码必须在6~12位");
+        }
+        else{
+            $('#ForgetErrPassword').hide();
+            return true;
+        }
+    });
--- sskl/templates/pages/sskl_login.js
+++ sskl/templates/pages/sskl_login.js
@@ -1,8 +1,78 @@
 
-$("#login").click(function () {
-    // console.log("登录跳转");  出现闪烁 也就是两次的效果
-    location.href = "http://v3.bootcss.com/";
+//登录页面
+$("#login").click(function(){
+//     $.ajax({
+//        type:get,
+//        url:"",
+//        data:"",
+//        success:function(){
+//
+//        },
+//        err:function(){
+//
+//        }
+//    });
 });
-$(function () {
-    $("#tabs").tabs();
+
+
+
+//    注册功能页面start
+    $("#register").click(function () {
+        console.log("点击了下一步");
+        //location.href="https://www.baidu.com/";
+        location.href = "sskl_netWork.html";
+    });
+$(".loginInUl>li").click(function(){
+    $(this).css("backgroundColor","#fff").siblings("li").css("backgroundColor","#dde0e0");
 });
+    //  发送验证码
+    $("#getCode").click(function () {
+        var code = 60;
+        $(this).addClass("getCodeHover"); //移除click
+        $("#getCode").html(code + "S");
+        var timer = setInterval(function () {
+            code--;
+            $("#getCode").html(code + "S");
+            if (code == 0) {
+                $("#getCode").html("再次获取验证码").removeClass("getCodeHover");
+                clearInterval(timer)
+            }
+        }, 1000)
+    });
+
+
+    //验证手机号码
+    $("#phoneNum").blur(function(){
+       var phoneNum = $(this).val();
+        if (!(/^1[3|4|5|8][0-9]\d{4,8}$/.test(phoneNum))) {
+            $('#MesErr').show().text("您输入的号码有误");;
+            $("#phoneNum").focus();
+            return false;
+        }else{
+             $("#MesErr").hide();
+        }
+    });
+
+//    验证6-12位数的密码
+    $("#getpassWord").blur(function(){
+        var upwd= $.trim($('#getpassWord').val());
+        var regUpwd=/^[A-Za-z0-9]{6,12}$/;
+        if(!upwd){//当密码为空的时候
+            $('#MesErr').show().text("密码不能为空");
+            return false;
+        }
+        else if(!regUpwd.test(upwd)){
+            $('#MesErr').show().text("密码必须在6~12位");
+        }
+        else{
+            $('#MesErr').hide();
+            return true;
+        }
+    });
+
+//点击下一步操作
+    $("#register").click(function(){
+
+    });
+
+//    注册功能页面start
